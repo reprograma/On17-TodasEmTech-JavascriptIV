b617d8336455aad08f62f4585b8cefccf41c105a
Larissa da Silva de Oliveira <larissasilvaoliveira27@gmail.com>
2022-07-13 19:18:31 -0300

    exercicio estilizado com CSS

--- Desafios/larissa-oliveira/Ghibli/script.js
+++ Desafios/larissa-oliveira/Ghibli/script.js
@@ -11,12 +11,22 @@ async function buscarFilmes() {
   }
 }
 function renderizarFilmes(catalogo) {
+  const divTituloSite = document.createElement("div");
+  divTituloSite.setAttribute("class", "div-titulo");
+  container.appendChild(divTituloSite);
+
+  const tituloSite = document.createElement("h1");
+  tituloSite.innerHTML = " Studio Ghibi";
+  tituloSite.setAttribute("class", "titulo-site");
+  divTituloSite.appendChild(tituloSite);
+
   catalogo.map((filme) => {
     const card = document.createElement("div");
     card.setAttribute("class", "card");
     container.appendChild(card);
 
     const titulo = document.createElement("h2");
+    titulo.setAttribute("class", "titulo");
     titulo.innerText = filme.title;
     card.appendChild(titulo);
 
@@ -27,7 +37,30 @@ function renderizarFilmes(catalogo) {
     const ano = document.createElement("p");
     ano.innerHTML = `<b>Ano de lançamento: </b> ${filme.release_date}`;
     card.appendChild(ano);
+
+    const diretor = document.createElement ("p");
+    diretor.innerHTML = `<b> Diretor: </b> ${filme.director}`;
+    card.appendChild(diretor);
+
+
+    const descricao = document.createElement ("p");
+    descricao.setAttribute("class", "descricao");
+    descricao.innerHTML = `<b> Descrição: </b> ${filme.description}`;
+    card.appendChild(descricao);
+
+
+
   });
+
+  const footer = document.createElement("div");
+  footer.setAttribute("class", "footer");
+  container.appendChild(footer);
+
+  const textoFooter = document.createElement("p");
+  textoFooter.innerHTML = "Criado por Larissa Oliveira";
+  textoFooter.setAttribute("class", "texto-footer");
+  footer.appendChild(textoFooter);
+
 }
 
 buscarFilmes();
